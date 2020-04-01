952d6dc9a5a83d720d89a18daf6e58a8c3350d26
Teodor Sava <mice.graff@gmail.com>
2020-04-01 01:17:45 +0100

    Added Basic Movement

---
+++ SOTONGameUnity/Assets/Scripts/movementScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -5f;
+    public float jumpStrength = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float x;
+    Vector3 velocity;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y< 0)
+        {
+            x = Input.GetAxis("Horizontal");
+            velocity.y = -2f;
+        }
+
+       
+
+
+
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right* x+ transform.forward* z;
+
+        controller.Move(move* speed* Time.deltaTime);
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpStrength * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
