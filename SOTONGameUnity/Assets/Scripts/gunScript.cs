5a488e0aac85dd168fa13489f00c04f810640069
Teodor Sava <mice.graff@gmail.com>
2020-04-02 06:42:19 +0100

    Laser is now lasering

--- SOTONGameUnity/Assets/Scripts/gunScript.cs
+++ SOTONGameUnity/Assets/Scripts/gunScript.cs
@@ -7,7 +7,7 @@ public class gunScript : MonoBehaviour
     public ParticleSystem particle;
 
     public float damage = 10f;
-    public float range = 100f;
+    public float range = 10500f;
 
     public Camera fpsCamera;
 
@@ -31,14 +31,28 @@ public class gunScript : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
+        drawLine(fpsCamera.transform.position, fpsCamera.transform.forward * 2000f, Color.green, 0.2f);
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
-            Debug.DrawRay(fpsCamera.transform.position, fpsCamera.transform.forward* 10000f, Color.cyan, Time.deltaTime, false);
+            
             getHitScript target= hit.transform.GetComponent<getHitScript>();
             if (target!=null)
             {
                 target.TakeDamage(damage);
             }
         }
+    }
+
+    void drawLine( Vector3 start, Vector3 end, Color color, float duration= 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.SetColors(color, color);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
     }
 }
