56273750e3a16637a5653d7228ec183aa7beeeeb
Teodor Sava <mice.graff@gmail.com>
2020-04-02 06:00:57 +0100

    yes

---
+++ SOTONGameUnity/Assets/Scripts/gunScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunScript : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            particle.Play();
+            Debug.Log("leftclick");
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.DrawRay(fpsCamera.transform.position, fpsCamera.transform.forward* 10000f, Color.cyan, Time.deltaTime, false);
+            getHitScript target= hit.transform.GetComponent<getHitScript>();
+            if (target!=null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ SOTONGameUnity/Assets/getHitScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getHitScript : MonoBehaviour
+{
+    public float hp = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float amount)
+    {
+        hp -= amount;
+        if (hp <=0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
