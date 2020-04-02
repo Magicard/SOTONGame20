41ad90c374e890f6fd7cc3e21e7c018f08334d0e
Teodor Sava <mice.graff@gmail.com>
2020-04-02 19:06:35 +0100

    Revert "Revert "Bunchy of struff""
    This reverts commit ee521be5435056856490df7cac89457a515cfaf8.

--- SOTONGameUnity/Assets/Scripts/gunScript.cs
+++ SOTONGameUnity/Assets/Scripts/gunScript.cs
@@ -8,8 +8,14 @@ public class gunScript : MonoBehaviour
 
     public float damage = 10f;
     public float range = 10500f;
-
+    public Material transparent;
+    public GameObject gun;
+    public AudioSource soundEffect;
+    public AudioSource soundEffect2;
+    public bool slowMo;
     public Camera fpsCamera;
+    public GameObject player;
+    public bool isGround;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +28,46 @@ public class gunScript : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            particle.Play();
-            Debug.Log("leftclick");
-            Shoot();
+            if (!soundEffect.isPlaying)
+            {
+                particle.Play();
+                Debug.Log("leftclick");
+                Shoot();
+            }
+        }
+
+ 
+
+        if (Input.GetButton("Fire2")&& !player.GetComponent<movementScript>().isGrounded)
+        {
+            soundEffect.Play(1);
+            Time.timeScale = 0.1f;
+            slowMo = true;
+        }
+        else
+        {
+            soundEffect2.Stop();
+            slowMo = false;
+            Time.timeScale = 1f;
         }
     }
+    
 
     void Shoot()
     {
         RaycastHit hit;
-        drawLine(fpsCamera.transform.position, fpsCamera.transform.forward * 2000f, Color.green, 0.2f);
+        if (slowMo)
+        {
+            soundEffect.pitch = 0.75f;
+        }
+        else
+        {
+            soundEffect.pitch = 1f;
+        }
+        soundEffect.Play(0);
+        drawLine(fpsCamera.transform.position, fpsCamera.transform.forward * 2000f, Color.green, 0.5f);
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
-            
             getHitScript target= hit.transform.GetComponent<getHitScript>();
             if (target!=null)
             {
@@ -43,8 +76,11 @@ public class gunScript : MonoBehaviour
         }
     }
 
-    void drawLine( Vector3 start, Vector3 end, Color color, float duration= 0.2f)
+    void drawLine( Vector3 start, Vector3 end, Color color, float duration= 0.5f)
     {
+      
+        
+
         GameObject myLine = new GameObject();
         myLine.transform.position = start;
         myLine.AddComponent<LineRenderer>();
@@ -53,6 +89,8 @@ public class gunScript : MonoBehaviour
         lr.SetWidth(0.1f, 0.1f);
         lr.SetPosition(0, start);
         lr.SetPosition(1, end);
+        Material whiteDiffuseMat = new Material(Shader.Find("Unlit/Texture"));
+        lr.material = transparent;
         GameObject.Destroy(myLine, duration);
     }
 }
