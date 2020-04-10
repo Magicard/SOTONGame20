59aab8f7e5193437f939608c607976a23727b0a2
Teodor Sava <mice.graff@gmail.com>
2020-04-10 17:54:56 +0100

    Minigun

---
+++ SOTONGameUnity/Assets/Scripts/minigunScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class minigunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public AudioSource soundEffect;
+    public GameObject muzzle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            
+            Shoot();
+        }
+        else
+        {
+            soundEffect.Stop();
+        }
+    }
+
+    void Shoot()
+    {
+        if (!soundEffect.isPlaying)
+        {
+            soundEffect.Play(0);
+        }
+        Instantiate(muzzle, gameObject.transform.position, gameObject.transform.rotation);
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            getHitScript target = hit.transform.GetComponent<getHitScript>();
+            if (target != null)
+            {
+                Debug.Log(target);
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
+}
--- SOTONGameUnity/Assets/gunSwitchScript.cs
+++ SOTONGameUnity/Assets/gunSwitchScript.cs
@@ -7,6 +7,9 @@ public class gunSwitchScript : MonoBehaviour
     public GameObject rail;
     public GameObject shotgun;
     public GameObject minigun;
+    public Animator anim1;
+    public Animator anim2;
+    public Animator anim3;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +19,29 @@ public class gunSwitchScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("1"))
+        if (!(anim1.GetCurrentAnimatorStateInfo(0).IsTag("1") || anim2.GetCurrentAnimatorStateInfo(0).IsTag("1") || anim3.GetCurrentAnimatorStateInfo(0).IsTag("1")))
         {
-            rail.SetActive(false);
-            shotgun.SetActive(false);
-            minigun.SetActive(false);
-            shotgun.SetActive(true);
-        }
-        if (Input.GetKeyDown("2"))
-        {
-            rail.SetActive(false);
-            shotgun.SetActive(false);
-            minigun.SetActive(false);
-            minigun.SetActive(true);
-        }
-        if (Input.GetKeyDown("3"))
-        {
-            rail.SetActive(false);
-            shotgun.SetActive(false);
-            minigun.SetActive(false);
-            rail.SetActive(true);
+            if (Input.GetKeyDown("1"))
+            {
+                rail.SetActive(false);
+                shotgun.SetActive(false);
+                minigun.SetActive(false);
+                shotgun.SetActive(true);
+            }
+            if (Input.GetKeyDown("2"))
+            {
+                rail.SetActive(false);
+                shotgun.SetActive(false);
+                minigun.SetActive(false);
+                minigun.SetActive(true);
+            }
+            if (Input.GetKeyDown("3"))
+            {
+                rail.SetActive(false);
+                shotgun.SetActive(false);
+                minigun.SetActive(false);
+                rail.SetActive(true);
+            }
         }
     }
 }
