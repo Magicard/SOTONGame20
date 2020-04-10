62504a9ad1b6b980f59ed0caaee2ec457bad68b7
DrinkinTea <33289598+DrinkinTea@users.noreply.github.com>
2020-04-10 18:55:05 +0100

    wdadwadwada

---
+++ SOTONGameUnity/Assets/BulletCol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCol : MonoBehaviour
+{
+	public float damage = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+ 
+
+void OnCollisionEnter (Collision other) {
+		Debug.Log("FUCK");
+
+		getHitScript target = other.transform.GetComponent<getHitScript>();
+
+		if (target != null)
+		{
+			Debug.Log("PENIS");
+			Debug.Log(target);
+			target.TakeDamage(damage);
+		}
+	
+		}
+	}	
