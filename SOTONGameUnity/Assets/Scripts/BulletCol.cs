496edf7329bf9054de3620f984d5328365704e44
DrinkinTea <33289598+DrinkinTea@users.noreply.github.com>
2020-04-12 18:14:49 +0100

    Changes
    added life decay to bullets

---
+++ SOTONGameUnity/Assets/Scripts/BulletCol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCol : MonoBehaviour
+{
+	public float damage = 50f;
+	public float lifetime = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+ 
+	void Update ()
+	{
+		lifetime -= 1;
+		if (lifetime <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+void OnCollisionEnter (Collision other) {
+		Debug.Log("FUCK");
+
+		getHitScript target = other.transform.GetComponent<getHitScript>();
+
+		if (target != null)
+		{
+			Debug.Log("PENIS");
+			Debug.Log(target);
+			target.TakeDamage(damage);
+			Destroy(gameObject);
+		}
+
+
+	}	
+
+
+}
---
+++ SOTONGameUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float Health = 100f;
+	public float graceperiod = 0f;
+	public bool grace = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (grace == true)
+		{
+			graceperiod += 1.0f;
+		}
+
+		if (graceperiod > 100f)
+		{
+			grace = false;
+			graceperiod = 0.0f;
+		}
+			
+    }
+
+			void OnCollisionEnter (Collision hit)
+			{
+		if(hit.transform.gameObject.name == "Splitter" & (grace == false))
+				{
+			grace = true;
+					Health -= 25.0f;
+					
+				}
+			}
+
+			}
